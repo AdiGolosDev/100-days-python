0fe15ea7925e447b3a513d04556e96be63980428
Adi Golos <golos.adi.03@gmail.com>
2026-05-16 14:25:18 +0200

    100days-of-python-udemy course initial repo setup and day 1 completed

---
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 1/task/task.py
@@ -0,0 +1,5 @@
+print("Hello, this is the Band Name Generator App! \n\nWelcome!")
+city = input("What city did you grow up in?\n")
+pet = input("What is or was the name of your most loved pet?\n")
+
+print("Your band name could be: " + city + " " + pet + ", or: " + pet + " " + city)
