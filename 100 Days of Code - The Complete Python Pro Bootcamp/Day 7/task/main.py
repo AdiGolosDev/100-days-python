9dda7cbd6a476f5b45dae994640450518649a821
Adi Golos <golos.adi.03@gmail.com>
2026-05-23 12:59:25 +0200

    completed simple hangman game functionality

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 7/task/main.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 7/task/main.py
@@ -1,8 +1,50 @@
-word_list = ["aardvark", "baboon", "camel"]
+import random
 
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
+word_list = ["aardvark", "baboon", "camel", "omnipotent", "mystery", "puzzle", "hangman"]
 
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+chosen_word = random.choice(word_list)
+word_guess = []
 
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word. Print "Right" if it
-#  is, "Wrong" if it's not.
+for character in chosen_word:
+    word_guess.append("_")
+
+def fill(g):
+    i = 0
+    for char in chosen_word:
+        if g == char:
+            word_guess[i] = g
+            i += 1
+        else:
+            i += 1
+
+def print_progress():
+    a = ""
+    for char in word_guess:
+        a += char
+    return a
+guess_count = int(input("How many guesses would you like to allow yourself? \nThe minimum number of guesses is 5"))
+if guess_count < 5:
+    guess_count = 5
+    print("Minimum number of guesses will be applied...")
+
+print("Your current progress is: " + print_progress())
+while print_progress() != chosen_word and guess_count > 0:
+    print(f"Attempts remaining: {guess_count}")
+    guess = input("Please guess a letter! ").lower()
+    if guess in word_guess:
+        print("You already guessed this letter!")
+    else:
+        guess_count -= 1
+
+    if guess in chosen_word:
+        print("Right, this letter is in the chosen word!")
+        fill(guess)
+    else:
+        print("Wrong, this letter is not in the chosen word!")
+
+    print("Your current guess progress is: " + print_progress())
+
+if guess_count < 1 and print_progress() != chosen_word:
+    print("You lose!")
+else:
+    print("You win!")
