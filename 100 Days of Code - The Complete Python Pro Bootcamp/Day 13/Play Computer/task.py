289e0b13fa1a0c3c60f9e56384194cae7999ef61
Adi Golos <golos.adi.03@gmail.com>
2026-05-29 21:59:11 +0200

    completed day 13: learned about debugging and using IDE debuger

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 13/Play Computer/task.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 13/Play Computer/task.py
@@ -1,6 +1,6 @@
 year = int(input("What's your year of birth?"))
 
-if year > 1980 and year < 1994:
-    print("You are a millennial.")
+if year > 1980 and year <= 1994: # has to actually include 1994 so that it doesn't bug out
+    print("You are a millennial.") #idk if 1994 is millenial or not
 elif year > 1994:
     print("You are a Gen Z.")
