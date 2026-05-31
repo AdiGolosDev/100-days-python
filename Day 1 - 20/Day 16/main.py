6542c2345635569b9eda7db0bd5cbaf7f9f8ab03
Adi Golos <golos.adi.03@gmail.com>
2026-05-31 23:24:55 +0200

    completed day 16 oop coffee machine project

--- Day 1 - 20/Day 16/main.py
+++ Day 1 - 20/Day 16/main.py
@@ -8,3 +8,18 @@ money_machine = MoneyMachine()
 
 on = True
 while on:
+    user_choice = input(f"Hello, what drink would you like?:\n{menu.get_items()}\n").lower()
+
+    if user_choice == "off":
+        on = False
+        break
+    elif user_choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    elif coffee_maker.is_resource_sufficient(menu.find_drink(user_choice)):
+        print(f"The cost of {user_choice} is ${menu.find_drink(user_choice).cost}.")
+        payment_made = money_machine.make_payment(menu.find_drink(user_choice).cost)
+        if payment_made:
+            coffee_maker.make_coffee(menu.find_drink(user_choice))
+    else:
+        print("Sorry, something went wrong.")
