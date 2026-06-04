fc1d3a521dfe54127bee010571294105ca282593
Adi Golos <golos.adi.03@gmail.com>
2026-06-04 11:57:49 +0200

    completed day 17 oop quiz program; now randomizes list of questions and asks until every question is covered

--- Day 1 - 20/Day 17/main.py
+++ Day 1 - 20/Day 17/main.py
@@ -1,8 +1,18 @@
-from data.py import question_data
-import question_model.py
+from data import question_data
+import question_model
+import quiz_brain
+import random
 
 question_bank = []
 
 for item in question_data:
     q = question_model.Question(item["text"], item["answer"])
     question_bank.append(q)
+
+random.shuffle(question_bank)
+
+brain_one = quiz_brain.Quiz_Brain(0, 0, question_bank)
+while brain_one.still_has_questions():
+    brain_one.next_question()
+
+print(f"You've completed the quiz.\nYour final score was: {brain_one.score}/{brain_one.number}")
--- Day 1 - 20/Day 17/quiz_brain.py
+++ Day 1 - 20/Day 17/quiz_brain.py
@@ -1,8 +1,32 @@
 class Quiz_Brain:
-    def __init__(self, number, list):
+    def __init__(self, number, score, list):
         self.number = 0
+        self.score = 0
         self.list = list
 
     def next_question(self):
         question = self.list[self.number]
-        input (f"Q.{self.number}: {question}. (True / False)? \n")
+        user_answer = input(f"Q.{self.number + 1}: {question.text} (True / False)? \n").lower()
+        if user_answer != "true" and user_answer != "false":
+            print("Please enter 'True' or 'False'.")
+            self.next_question()
+        else:
+            self.check_answer(user_answer)
+    
+    def check_answer(self, user_answer):
+        correct_answer = self.list[self.number].answer.lower()
+        if user_answer == correct_answer:
+            self.score += 1
+            print(f"You got it right!\nThe correct answer was: {correct_answer}")
+            self.number += 1
+            print(f"Your current score is: {self.score}/{self.number}\n")
+        else:
+            print(f"That's wrong.\nThe correct answer was: {correct_answer}")
+            self.number += 1
+            print(f"Your current score is: {self.score}/{self.number}\n")
+
+    def still_has_questions(self):
+        if self.number < len(self.list)-1:
+            return True
+        else:
+            return False
