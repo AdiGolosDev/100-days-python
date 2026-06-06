d5ad56d9e3f8974badf29e97f8a68feea7be3ac9
Adi Golos <golos.adi.03@gmail.com>
2026-06-06 12:57:29 +0200

    completed day 18 turtle 2 challenge; learned penup/pendown methods

--- Day 1 - 20/Day 18/main.py
+++ Day 1 - 20/Day 18/main.py
@@ -3,11 +3,14 @@ from turtle import Turtle, Screen
 klokan = Turtle()
 klokan.shape("turtle")
 klokan.color("green")
-
 for i in range(4):
-    klokan.forward(100)
-    klokan.left(90)
-
+    klokan.right(90)
+    for i in range(10):
+        klokan.pendown()
+        klokan.forward(10)
+        klokan.penup()
+        klokan.forward(10)
+        
 
 
 
