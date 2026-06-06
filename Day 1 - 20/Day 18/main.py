8a9b4d7a0cdfd873afb67d651a8cbff9d31ef713
Adi Golos <golos.adi.03@gmail.com>
2026-06-06 12:47:02 +0200

    started day 18; turtle module and importing practice

---
+++ Day 1 - 20/Day 18/main.py
@@ -0,0 +1,16 @@
+from turtle import Turtle, Screen
+
+klokan = Turtle()
+klokan.shape("turtle")
+klokan.color("green")
+
+for i in range(4):
+    klokan.forward(100)
+    klokan.left(90)
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
