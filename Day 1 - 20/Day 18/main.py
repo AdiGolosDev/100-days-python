7fe14b656b81da718699e7e68584110865a17053
Adi Golos <golos.adi.03@gmail.com>
2026-06-06 13:30:19 +0200

    added change_color(), draw_gons() and random_walk() functions corresponding to day 18 tasks

--- Day 1 - 20/Day 18/main.py
+++ Day 1 - 20/Day 18/main.py
@@ -1,19 +1,48 @@
 from turtle import Turtle, Screen
+import random
+
+screen = Screen()
+screen.colormode(255)
+
+directions = [0, 90, 180, 270]
 
 klokan = Turtle()
 klokan.shape("turtle")
-klokan.color("green")
-for i in range(4):
-    klokan.right(90)
-    for i in range(10):
-        klokan.pendown()
-        klokan.forward(10)
-        klokan.penup()
-        klokan.forward(10)
+klokan.width(10)
+
+def draw_dashed_line():
+    for i in range(4):
+        klokan.right(90)
+        for i in range(10):
+            klokan.pendown()
+            klokan.forward(10)
+            klokan.penup()
+            klokan.forward(10)
+
+def change_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return (r, g, b)
+
+def draw_gons():   #3 - 22 gons because everything ends on gon(pentagon, hexagon...)
+    i = 3
+    while i < 23:
+        angle = 360 / i
+        for _ in range(i):
+            klokan.right(angle)
+            klokan.forward(50)
+        klokan.color(change_color())
+        i += 1
+
+def random_walk():
+    for _ in range(200):
+        klokan.color(change_color())
+        klokan.forward(22)
+        klokan.setheading(random.choice(directions))
         
 
+klokan.speed(0)
+random_walk()
 
-
-
-screen = Screen()
 screen.exitonclick()
