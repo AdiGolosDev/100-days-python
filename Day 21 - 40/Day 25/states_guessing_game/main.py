1768ed9665af18474a737d85c53cea4db0708f02
Adi Golos <golos.adi.03@gmail.com>
2026-06-17 19:02:19 +0200

    added comments shortly describing code sections and other simple cleanup

--- Day 21 - 40/Day 25/states_guessing_game/main.py
+++ Day 21 - 40/Day 25/states_guessing_game/main.py
@@ -1,15 +1,18 @@
 import turtle
 import pandas
 
+# screen construction
 screen = turtle.Screen()
 screen.title("U.S. State Guessing Game")
 image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
+# reads csv file using pandas; makes list for state names
 states = pandas.read_csv("50_states.csv")
 state_names = states.state.to_list()
 
+# makes turtle for displaying text for correctly guessed state
 def make_text_turtle(x, y, state):
     new_turtle = turtle.Turtle()
     new_turtle.hideturtle()
@@ -17,9 +20,11 @@ def make_text_turtle(x, y, state):
     new_turtle.goto(x, y)
     new_turtle.write(state, align='center', font=('Arial', 8, 'normal'))
 
+# game on loop
 correct = 0
 game_on = True
 while game_on:
+    # once all 50 states are guessed, makes congratulations text
     if correct == 50:
         winning_turtle = turtle.Turtle()
         winning_turtle.penup()
@@ -27,7 +32,8 @@ while game_on:
         winning_turtle.goto(0, 250)
         winning_turtle.write("You Guessed Them All!\nCongratulations, You Win!!!", align='center', font=('Arial', 20, 'normal'))
         break
-
+    
+    # gets user input and turns it into Title Case
     user_input = screen.textinput(title=f"{correct}/50 - Guess the State", prompt="What other states do you know?")
     if user_input is None:
         continue
@@ -36,17 +42,12 @@ while game_on:
         continue
     answer = user_input.title()
 
+    # if not already guessed, makes turtle and increments correct score
     if answer in state_names:
         x = states.loc[states.state == answer, "x"].item()
         y = states.loc[states.state == answer, "y"].item()
         make_text_turtle(x, y, answer)
         state_names.remove(answer)
         correct += 1
-
-            
-
-
-
-
 
 screen.exitonclick()
