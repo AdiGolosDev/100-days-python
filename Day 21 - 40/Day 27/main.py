84958e28ecafa7a53f9566b2009b655746377473
Adi Golos <golos.adi.03@gmail.com>
2026-06-19 18:56:14 +0200

    started day 27; learning tkinter; created main.py for basic practice

---
+++ Day 21 - 40/Day 27/main.py
@@ -0,0 +1,14 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("My first Tkinter GUI program")
+window.minsize(width=500, height=300)
+
+# Label
+my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
+my_label.pack()
+
+
+
+
+window.mainloop()
