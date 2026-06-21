86799295cf70c1dc5b3544527661d83778551b83
Adi Golos <golos.adi.03@gmail.com>
2026-06-21 12:20:33 +0200

    finished timers, and start() function; count_down now calls start upon reaching 0 sec; left-to-do: finish reset function

--- Day 21 - 40/Day 28/pomodoro-start/main.py
+++ Day 21 - 40/Day 28/pomodoro-start/main.py
@@ -2,85 +2,74 @@ from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
-GREEN = "#9bdeac"
+GREEN = "#268b3f"
+DARK_GREEN = "#0a501c"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
-# on = False
-# def start():
-#     global on
-#     on = True
-#     timer()
-
-# def reset():
-#     global on
-#     global remaining_time
-#     on = False
-#     remaining_time = 0
-#     canvas.config(text="00:00")
-
-def start():
-    pass
-
 def reset():
     pass
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
-# def start_work():
-#     count_down(WORK_MIN * 60)
+def start():
+    global reps
+    reps += 1
+    if reps >= 8:
+        start_long_break()
+    elif reps % 2 == 0:
+        start_short_break()
+    else:
+        start_work()
 
-# def start_short_break():
-#     count_down(SHORT_BREAK_MIN * 60)
+def start_work():
+    timer.config(text="Time to Get Shit Done!", fg=RED)
+    count_down(WORK_MIN * 60)
 
-# def start_long_break():
-#     count_down(LONG_BREAK_MIN * 60)
-#     reset()
+def start_short_break():
+    timer.config(text=" # Short Break Time # ", fg=GREEN)
+    check_marks.config(text=check_marks.cget("text") + "✓")
+    count_down(SHORT_BREAK_MIN * 60)
 
-# def timer():
-#     while on:
-#         for _ in range(4):
-#             start_work()
-#             while True:
-#                 if remaining_time <= 0:
-#                     break
-#             check_marks.config(text=check_marks.cget("text") + "✓")
-#             start_short_break()
-#         start_long_break()
-
+def start_long_break():
+    check_marks.config(text=check_marks.cget("text") + "✓")
+    timer.config(text="Congratulations!\nYou must've done\nsomething useful today!", fg=DARK_GREEN, font=(FONT_NAME, 28, "italic"))
+    count_down(LONG_BREAK_MIN * 60)
+    reset()
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def remaining_time_to_string(time):
+    colon = ":"
+    if time % 2 == 1:
+        colon = " "
     min = int(time/60)
     if min <= 9:
-        string = "0" + str(min) + ":"
+        string = "0" + str(min) + colon
     else:
-        string = str(min) + ":"
+        string = str(min) + colon
     
-    if time%60 <= 9:
-        string += "0" + str(time%60)
+    sec = time % 60
+    if sec <= 9:
+        string += "0" + str(sec)
     else:
-        string += str(time%60)
+        string += str(sec)
     return string
-
-# def count_down(count):
-#     canvas.config(text=remaining_time_to_string(remaining_time))
-#     remaining_time -= 1
-#     window.after(1000, count_down, count - 1)
 
 def count_down(count):
     if count > 0:
         canvas.itemconfig(timer_text, text=remaining_time_to_string(count))
         window.after(1000, count_down, count - 1)
-
+    elif count == 0:
+        start()
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro Program")
-window.config(padx=100, pady=100, bg=YELLOW)
+window.config(padx=50, pady=100, bg=YELLOW, width=600, height=500)
 
 tomato_image = PhotoImage(file="tomato.png")
 canvas = Canvas(width=210, height=224, bg=YELLOW, highlightthickness=0)
@@ -88,12 +77,10 @@ canvas.create_image(103, 112, image=tomato_image)
 timer_text = canvas.create_text(100, 130, text=remaining_time_to_string(0), fill="white", font=(FONT_NAME, 34, "bold"))
 canvas.grid(column=1, row=1, pady=20, padx=40)
 
-count_down(20)
-
-timer = Label(text="Timer", fg="#000000", bg=YELLOW, font=(FONT_NAME, 48, "italic"))
+timer = Label(text="         Timer        ", fg="#000000", bg=YELLOW, font=(FONT_NAME, 48, "italic"))
 timer.grid(column=1, row=0)
 
-start_button = Button(text="start", fg="#000000", font=(FONT_NAME, 14, "italic"), command=reset, width=8, height=2, highlightthickness=2)
+start_button = Button(text="start", fg="#000000", font=(FONT_NAME, 14, "italic"), command=start, width=8, height=2, highlightthickness=2)
 start_button.grid(column=0, row=2)
 
 reset_button = Button(text="reset", fg="#000000", font=(FONT_NAME, 14, "italic"), command=reset, width=8, height=2, highlightthickness=2)
