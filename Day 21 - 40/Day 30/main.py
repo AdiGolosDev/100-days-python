bf67264bcb6064c309f1f29a6694cb68130a3e6c
Adi Golos <golos.adi.03@gmail.com>
2026-06-24 19:20:07 +0200

    day 30; learning more json, errors, try and excepts

---
+++ Day 21 - 40/Day 30/main.py
@@ -0,0 +1,18 @@
+#  This results with traceback error/ no file found:
+#  with open("a_file.txt") as file:
+#     file.read()
+
+# try:
+# except:
+# else:
+# finally:
+
+try:
+    file = open("a_file.txt")
+    a_dictionary = {"key": "value"}
+    print(a_dictionary["this key doesn't exist"])
+except FileNotFoundError:
+    file = open("a_file.txt", "w")
+    file.write("Something")
+except KeyError as error_message:
+    print(f"The key {error_message} does not exist.")
