2cdac14090c1d7cce515285a4ed8adb237d090b8
Adi Golos <golos.adi.03@gmail.com>
2026-06-24 19:55:08 +0200

    day 30 more practice; added exception handling to NATO alphabet project

--- Day 21 - 40/Day 26/NATO-alphabet-start/main.py
+++ Day 21 - 40/Day 26/NATO-alphabet-start/main.py
@@ -9,8 +9,13 @@ while on:
     if word == "/end":
         on = False
         break
+
     word_list = [letter.upper() for letter in word]
-    word_nato_list = [f"{letter} = {dict[letter]}" for letter in word_list]
+    try:
+        word_nato_list = [f"{letter} = {dict[letter]}" for letter in word_list]
+    except KeyError:
+        print("Sorry, only use letters in the alphabet please.")
+        continue
+
     for item in word_nato_list:
         print(item)
-
--- Day 21 - 40/Day 30/main.py
+++ Day 21 - 40/Day 30/main.py
@@ -7,12 +7,27 @@
 # else:
 # finally:
 
-try:
-    file = open("a_file.txt")
-    a_dictionary = {"key": "value"}
-    print(a_dictionary["this key doesn't exist"])
-except FileNotFoundError:
-    file = open("a_file.txt", "w")
-    file.write("Something")
-except KeyError as error_message:
-    print(f"The key {error_message} does not exist.")
+# try:
+#     file = open("a_file.txt")
+#     a_dictionary = {"key": "value"}
+#     print(a_dictionary["this key doesn't exist"])
+# except FileNotFoundError:
+#     file = open("a_file.txt", "w")
+#     file.write("Something")
+# except KeyError as error_message:
+#     print(f"The key {error_message} does not exist.")
+# else:
+#     content = file.read()
+#     print(content)
+# finally: # not often used, but can be used to execute something despite everything else not working
+#     # raise TypeError # can create your own errors
+#     raise TypeError("This is an error that I made up")
+
+height = float(input("Height: "))
+weight = int(input("Weight: "))
+
+if height > 3:
+    raise ValueError("C'mon man you're not godzilla.")
+
+bmi = weight / height ** 2
+print(bmi)
