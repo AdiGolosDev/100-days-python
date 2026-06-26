f202c1b624274d648a100574a99083310aa19204
Adi Golos <golos.adi.03@gmail.com>
2026-06-26 22:26:44 +0200

    get_quote() reads quote from quotes.txt; send_quote(quote) sends the quote to an email

--- Day 21 - 40/Day 32 birthday/main.py
+++ Day 21 - 40/Day 32 birthday/main.py
@@ -1,6 +1,8 @@
 import smtplib
+import random
 import json
 import os
+import datetime as dt
 
 CREDENTIALS_FILE = "email_credentials.json"
 
@@ -29,14 +31,25 @@ def get_email_by_index(index: int) -> tuple[str, str | None]:
     password = credentials[email]
     return email, password
 
-from_email, from_email_pass = get_email_by_index(0)
-to_email, _ = get_email_by_index(2)
+def send_quote(quote):
+    from_email, from_email_pass = get_email_by_index(0)
+    to_email, _ = get_email_by_index(1)
 
-with smtplib.SMTP("smtp.gmail.com", 587) as connection:
-    connection.starttls()
-    connection.login(user=from_email, password=from_email_pass)
-    connection.sendmail(from_addr=from_email, to_addrs=to_email, msg="Subject: Kako smo dobri programeri\n\nEvo sada i subject stavljamo")
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(user=from_email, password=from_email_pass)
+        connection.sendmail(from_addr=from_email, to_addrs=to_email, msg=f"Quote of the Day\n\n{quote}")
 
-# import datetime as dt
+def get_quote():
+    with open("quotes.txt") as f:
+        quotes_list = f.readlines()
+        quote = random.choice(quotes_list)
+    
+    return quote
 
-# now = dt.datetime.now()
+now = dt.datetime.now()
+weekday = now.weekday()
+quote = get_quote()
+
+if weekday == 4:
+    send_quote(quote)
