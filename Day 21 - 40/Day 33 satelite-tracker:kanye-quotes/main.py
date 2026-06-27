647055f3c3a3df580f72a6639a506729d424c932
Adi Golos <golos.adi.03@gmail.com>
2026-06-27 16:19:16 +0200

    completed kanye quotes program with kanye rest api

--- Day 21 - 40/Day 33 satelite-tracker:kanye-quotes/main.py
+++ Day 21 - 40/Day 33 satelite-tracker:kanye-quotes/main.py
@@ -1,8 +1,9 @@
 import requests
-import json
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status() # raises http error if one occurs
 
-data = response.json()["iss_position"]["longitude"]
-print(data)
+data = response.json()
+longitude = data["iss_position"]["longitude"]
+
+print(longitude)
