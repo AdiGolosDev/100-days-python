174223b29ccc77273669c28842a39d8a67fae1c3
Adi Golos <golos.adi.03@gmail.com>
2026-06-28 17:50:00 +0200

    setting up day 34 quiz project; completed fetching questions from open trivia api

---
+++ Day 21 - 40/Day 34 GUI-quiz/data.py
@@ -0,0 +1,18 @@
+import requests
+
+amount = 10
+
+parameters = {
+    "amount": amount,
+    "type": "boolean"
+}
+
+response = requests.get(url="https://opentdb.com/api.php", params=parameters)
+response.raise_for_status()
+
+data = response.json()
+questions = data["results"]
+
+for question in questions:
+    print(question)
+    
