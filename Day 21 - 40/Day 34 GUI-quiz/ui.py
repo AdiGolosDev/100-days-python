f469624ed1c36c7d0c2ec85edce54e9f8aef652e
Adi Golos <golos.adi.03@gmail.com>
2026-06-28 19:08:34 +0200

    cleaned up code

--- Day 21 - 40/Day 34 GUI-quiz/ui.py
+++ Day 21 - 40/Day 34 GUI-quiz/ui.py
@@ -59,4 +59,3 @@ class QuizInterface:
         if hasattr(sys, '_MEIPASS'):
             return os.path.join(sys._MEIPASS, relative_path)
         return os.path.join(os.path.dirname(__file__), relative_path)
-
