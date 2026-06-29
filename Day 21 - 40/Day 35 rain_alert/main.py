f460e453f99be38075895718870ec5a51ff36b7a
Adi Golos <golos.adi.03@gmail.com>
2026-06-29 22:28:37 +0200

    day 35 project now checks if it will rain at entered location within the next 15 hours

--- Day 21 - 40/Day 35 rain_alert/main.py
+++ Day 21 - 40/Day 35 rain_alert/main.py
@@ -5,9 +5,13 @@ import os
 load_dotenv()
 api_key = os.getenv("API_KEY")
 
-#japan
-LAT = 34.623232
-LON = 133.878453
+#okinawa
+# LAT = 34.623232
+# LON = 133.878453
+
+#dublin
+LAT = 53.349804
+LON = -6.260310
 
 params = {
     "lat": LAT,
@@ -18,4 +22,16 @@ params = {
 response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast", params=params)
 response.raise_for_status()
 
-print(response)
+data = response.json()
+
+rain = False
+for forecast in data["list"][0:5]:
+    weather_id = forecast["weather"][0]["id"]
+    description = forecast["weather"][0]["description"]
+    if "rain" in description.lower() or "drizzle" in description.lower():
+        rain = True
+
+if rain:
+    print("There will be rain within the next 15 hours")
+else:
+    print("There won't be rain in the next 15 hours")
