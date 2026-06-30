52b2e0a11db9412dce77dd71c231a8864774187b
Adi Golos <golos.adi.03@gmail.com>
2026-06-30 19:24:12 +0200

    program now sends message to whatsapp using twilio

--- Day 21 - 40/Day 35 rain_alert/main.py
+++ Day 21 - 40/Day 35 rain_alert/main.py
@@ -1,9 +1,15 @@
 import requests
 from dotenv import load_dotenv
 import os
+from twilio.rest import Client
 
 load_dotenv()
 api_key = os.getenv("API_KEY")
+auth_token = os.getenv("AUTH_TOKEN")
+phone_num = os.getenv("PHONE")
+account_sid = os.getenv("ACCOUNT_SID")
+
+client = Client(account_sid, auth_token)
 
 #okinawa
 # LAT = 34.623232
@@ -21,17 +27,25 @@ params = {
 
 response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast", params=params)
 response.raise_for_status()
-
 data = response.json()
 
 rain = False
+# angela's(100 days python instructor) code checks if weather id is < 700 which includes snow and everything
+# if it snows i ain't bringing no umbrella
 for forecast in data["list"][0:5]:
-    weather_id = forecast["weather"][0]["id"]
+    # weather_id = forecast["weather"][0]["id"] i think i don't even need this for my own code to work
     description = forecast["weather"][0]["description"]
     if "rain" in description.lower() or "drizzle" in description.lower():
         rain = True
 
 if rain:
-    print("There will be rain within the next 15 hours")
+    text = "It will rain in the next 15 hours, bring an umbrella if you are made out of sugar..."
 else:
-    print("There won't be rain in the next 15 hours")
+    text = "There won't be rain in the next 15 hours. No need to think of bringing an umbrella."
+
+message = client.messages.create(
+  from_='whatsapp:+14155238886',
+  body=text,
+  to=phone_num
+)
+print(message.sid)
