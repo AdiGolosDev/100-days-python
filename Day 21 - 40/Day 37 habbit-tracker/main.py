730151f7bf6caef4c989e1b9e7fa6eebe92b53a8
Adi Golos <golos.adi.03@gmail.com>
2026-07-02 19:20:09 +0200

    formated today's time, added pixel to graph

--- Day 21 - 40/Day 37 habbit-tracker/main.py
+++ Day 21 - 40/Day 37 habbit-tracker/main.py
@@ -1,13 +1,19 @@
 import requests
 from dotenv import load_dotenv
 import os
+from datetime import datetime
 
 load_dotenv()
+
+t = datetime.now()
+today = str(t).split(" ")[0].replace("-", "")
 
 pixela_endpoint = "https://pixe.la/v1/users"
 TOKEN = os.getenv("TOKEN")
 USERNAME = os.getenv("USERNAME")
+GRAPH_ID = "read"
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+pixel_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
 
 user_params = {
     "token": TOKEN,
@@ -17,7 +23,7 @@ user_params = {
 }
 
 graph_config = {
-    "id": "graph2",
+    "id": GRAPH_ID,
     "name": "Reading Graph",
     "unit": "pg",
     "type": "int",
@@ -28,12 +34,14 @@ headers = {
     "X-USER-TOKEN": TOKEN
 }
 
+pixel_config = {
+    "date": today,
+    "quantity": "20"
+}
+
 # requests.post(url=pixela_endpoint, json=user_params)
-response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+response = requests.post(url=pixel_endpoint, json=pixel_config, headers=headers)
 print(response)
 data = response.json()
 print(data["message"])
-
-
-
-
