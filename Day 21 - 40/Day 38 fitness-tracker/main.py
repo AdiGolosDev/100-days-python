4a8ba1cd122304ac7d898012d5176205865b028d
Adi Golos <golos.adi.03@gmail.com>
2026-07-04 21:05:30 +0200

    completed day 38

--- Day 21 - 40/Day 38 fitness-tracker/main.py
+++ Day 21 - 40/Day 38 fitness-tracker/main.py
@@ -7,12 +7,16 @@ load_dotenv()
 
 xapp_id = os.getenv("XAPP_ID")
 xapp_key = os.getenv("XAPP_KEY")
+sheety_key = os.getenv("SHEETY_KEY")
 
 base_url = "https://app.100daysofpython.dev"
 post_url = f"{base_url}/v1/nutrition/natural/exercise"
+sheety_url = "https://api.sheety.co/b9282af21c6f87a9902d95863282b08a/fitnessTrackerPython/workouts"
 
 # query = input("Please describe the exercise you completed:\n")
-query = "swam for 1 hours"
+query = "played basketball for 3 hours"
+date = datetime.now().strftime("%d/%m/%Y")
+time = datetime.now().strftime("%H:%M:%S")
 
 body = {
     "query": query,
@@ -28,4 +32,27 @@ headers = {
 }
 
 response = requests.post(url=post_url, json=body, headers=headers)
-print(response.json()["exercises"])
+exercise = response.json()["exercises"][0]
+
+workout = {
+    "workout": {
+        "date": date,
+        "time": time,
+        "exercise": exercise["name"],
+        "duration": exercise["duration_min"],
+        "calories": exercise["nf_calories"]
+    }
+}
+
+sheety_header = {
+    "Authorization": f"Bearer {sheety_key}"
+}
+
+#post to sheety
+response = requests.post(url=sheety_url, json=workout, headers=sheety_header)
+print(response)
+
+#get from sheety
+response = requests.get(url=sheety_url, headers=sheety_header)
+print(response.json())
+
---
+++ Day 21 - 40/Day 38 fitness-tracker/tempCodeRunnerFile.py
@@ -0,0 +1,23 @@
+body = {
+#     "query": query,
+#     "weight_kg": 90,
+#     "height_cm": 188,
+#     "age": 22,
+#     "gender": "male"
+# }
+
+# headers = {
+#     "x-app-id": xapp_id,
+#     "x-app-key": xapp_key
+# }
+
+# sheet_body = {
+#     "Date": ,
+#     "Time": ,
+#     "Exercise": ,
+#     "Duration": ,
+#     "Calories":
+# }
+
+# response = requests.post(url=post_url, json=body, headers=headers)
+# print(response.json()["exercises"])
