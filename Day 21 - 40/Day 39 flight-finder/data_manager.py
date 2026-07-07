c9adc9279c18329672d86d41da69c21ea263c55b
Adi Golos <golos.adi.03@gmail.com>
2026-07-07 17:18:45 +0200

    first try at making data_manager class for sheety google sheet data managment

--- Day 21 - 40/Day 39 flight-finder/data_manager.py
+++ Day 21 - 40/Day 39 flight-finder/data_manager.py
@@ -1,3 +1,28 @@
+from requests_cache import CachedSession
+from datetime import timedelta
+
 class DataManager:
     #This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self, sheety_key):
+        self.sheety_key = sheety_key
+        self.sheety_url = "https://api.sheety.co/b9282af21c6f87a9902d95863282b08a/flightDeals/prices"
+        self.sheety_header = {"Authorization": f"Bearer {self.sheety_key}"}
+        self.session = CachedSession("data_cache", expire_after=timedelta(hours=2))
+
+    def get(self):
+        self.response = self.session.get(url=self.sheety_url, headers=self.sheety_header)
+        print(self.response)
+        return(self.response)
+
+    def post(self, city, iata_code, lowest_price):
+        self.price = {
+            "price": {
+                "city": city,
+                "iata code": iata_code,
+                "lowest price": lowest_price
+            }
+        }
+
+        self.response = self.session.post(url=self.sheety_url, json=self.price, headers=self.sheety_header)
+        print(self.response)
+        return(self.response)
