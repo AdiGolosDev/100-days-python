2300a9147aa1998d5706151429e5148ae5b1d046
Adi Golos <golos.adi.03@gmail.com>
2026-07-08 16:27:33 +0200

    flight_data format done, twilio whatsapp notification sender finished

--- Day 21 - 40/Day 39 flight-finder/notification_manager.py
+++ Day 21 - 40/Day 39 flight-finder/notification_manager.py
@@ -1,3 +1,14 @@
+import os
+from twilio.rest import Client
+
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
-    pass
+    def __init__(self):
+        self.client = Client(os.getenv("TWILIO_SID"), os.getenv("TWILIO_AUTH_TOKEN"))
+
+    def send_msg(self, message):
+        message = self.client.messages.create(
+            body=message,
+            from_=os.getenv("TWILIO_FROM_NUMBER"),
+            to=os.getenv("TWILIO_TO_NUMBER"),
+        )
+        print(message.status)
