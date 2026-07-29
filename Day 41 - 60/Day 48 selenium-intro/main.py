9b1c622e5e22a1633a5ca80d2402c756395306a2
Adi Golos <golos.adi.03@gmail.com>
2026-07-29 21:22:48 +0200

    more selenium practice; used dict comprehension to sort pyhon.org event names and dates

--- Day 41 - 60/Day 48 selenium-intro/main.py
+++ Day 41 - 60/Day 48 selenium-intro/main.py
@@ -1,10 +1,28 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://www.amazon.com")
+driver.get("https://www.python.org/")
 
+
+event_times = driver.find_elements(By.CSS_SELECTOR, value=".event-widget time")
+event_names = driver.find_elements(By.CSS_SELECTOR, value=".event-widget li a")
+
+events = {
+    i: {'time': time.text, 'name': name.text}
+    for i, (time, name) in enumerate(zip(event_times, event_names))
+}
+
+print(events)
+
+
+# product_name = driver.find_element(By.ID, value="productTitle").text
+# print(product_name)
+# # By.css_selector also exists
+# careers_link = driver.find_element(By.XPATH, value='//*[@id="navFooter"]/div[1]/div/div[1]/ul/li[1]/a')
+# print(careers_link.text)
 # driver.close() closes one tab
 driver.quit() # closes entire browser
