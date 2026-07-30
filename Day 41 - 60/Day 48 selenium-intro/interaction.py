a5fa6f736a9408b44fe494f81ee08c862b3f2ad4
Adi Golos <golos.adi.03@gmail.com>
2026-07-30 23:17:07 +0200

    solved day 48 challenge; cheesed it by using Keys.TAB

--- Day 41 - 60/Day 48 selenium-intro/interaction.py
+++ Day 41 - 60/Day 48 selenium-intro/interaction.py
@@ -6,17 +6,21 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(options=chrome_options)
-driver.get("https://en.wikipedia.org/wiki/Main_Page")
+# driver.get("https://en.wikipedia.org/wiki/Main_Page")
+driver.get("https://appbrewery.github.io/fake-newsletter-signup/")
 
-num_of_articles = driver.find_element(By.CSS_SELECTOR, value="#articlecount a")
-print(num_of_articles)
+first = driver.find_element(By.NAME, value="fName")
+first.send_keys("someFirstName", Keys.TAB, "someLastName", Keys.TAB, "some@Email.com", Keys.TAB, Keys.ENTER)
+
+# num_of_articles = driver.find_element(By.CSS_SELECTOR, value="#articlecount a")
+# print(num_of_articles)
 # num_of_articles.click()
 
-all_portals = driver.find_element(By.LINK_TEXT, value="Content portals")
+# all_portals = driver.find_element(By.LINK_TEXT, value="Content portals")
 # all_portals.click()
 
-search = driver.find_element(By.NAME, value="search")
-search.send_keys("Lord of Mysteries", Keys.ENTER)
+# search = driver.find_element(By.NAME, value="search")
+# search.send_keys("Lord of Mysteries", Keys.ENTER)
 
 input("press enter to quit")
 
