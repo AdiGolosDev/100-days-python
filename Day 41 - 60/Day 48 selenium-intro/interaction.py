4e451bb0fb4539f58cc73351208fe350057d02e0
Adi Golos <golos.adi.03@gmail.com>
2026-07-30 23:09:06 +0200

    practicing selenium click() and send_keys():

--- Day 41 - 60/Day 48 selenium-intro/interaction.py
+++ Day 41 - 60/Day 48 selenium-intro/interaction.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 chrome_options = webdriver.ChromeOptions()
@@ -7,7 +8,16 @@ chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://en.wikipedia.org/wiki/Main_Page")
 
-num_of_articles = driver.find_element(By.ID, value="mwDw").text
+num_of_articles = driver.find_element(By.CSS_SELECTOR, value="#articlecount a")
 print(num_of_articles)
+# num_of_articles.click()
+
+all_portals = driver.find_element(By.LINK_TEXT, value="Content portals")
+# all_portals.click()
+
+search = driver.find_element(By.NAME, value="search")
+search.send_keys("Lord of Mysteries", Keys.ENTER)
+
+input("press enter to quit")
 
 driver.quit()
---
+++ Day 41 - 60/Day 48 selenium-intro/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+search = driver.find_element(By.NAME, value="search")
+search.send_keys("Lord of Mysteries")
