0452dc10a3aed1b41074e8815791f718818059ae
Adi Golos <golos.adi.03@gmail.com>
2026-07-31 20:54:02 +0200

    automatically booked training session

--- Day 41 - 60/Day 49 exercise-routine-automation/main.py
+++ Day 41 - 60/Day 49 exercise-routine-automation/main.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
 from time import sleep, time
 import os
 
@@ -15,7 +18,27 @@ chrome_options.add_argument(f"--user-data-dir={user_data_dir}")
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://appbrewery.github.io/gym/")
 
+wait = WebDriverWait(driver, 2)
 
+login_button = driver.find_element(by=By.ID, value="login-button")
+login_button.click()
 
-input("Press enter to quit")
+login = driver.find_element(by=By.ID, value="email-input")
+login.send_keys(ACCOUNT_EMAIL, Keys.TAB, ACCOUNT_PASS, Keys.TAB, Keys.ENTER)
+
+wait.until(ec.presence_of_all_elements_located((By.ID, "schedule-page")))
+
+days = driver.find_elements(By.CLASS_NAME, value="Schedule_dayGroup__y79__")
+
+for day in days:
+    if "Tue" in day.text:
+        classes = day.find_elements(By.CLASS_NAME, value="ClassCard_card__KpCx5")
+        for c in classes:
+            if "6:00 PM" in c.text:
+                button = c.find_element(By.CSS_SELECTOR, value="button")
+                button.click()
+                break
+        break
+
+input("enter to quit")
 driver.quit()
