019c9fc73a537b84f5632e4ad11be3ab6f59393f
Adi Golos <golos.adi.03@gmail.com>
2026-07-31 21:34:54 +0200

    added booking summary

--- Day 41 - 60/Day 49 exercise-routine-automation/main.py
+++ Day 41 - 60/Day 49 exercise-routine-automation/main.py
@@ -28,6 +28,9 @@ login.send_keys(ACCOUNT_EMAIL, Keys.TAB, ACCOUNT_PASS, Keys.TAB, Keys.ENTER)
 
 wait.until(ec.presence_of_all_elements_located((By.ID, "schedule-page")))
 
+classes_booked = 0
+waitlists_joined = 0
+booked_waitlisted = 0
 days = driver.find_elements(By.CLASS_NAME, value="Schedule_dayGroup__y79__")
 
 for day in days:
@@ -35,10 +38,31 @@ for day in days:
         classes = day.find_elements(By.CLASS_NAME, value="ClassCard_card__KpCx5")
         for c in classes:
             if "6:00 PM" in c.text:
-                button = c.find_element(By.CSS_SELECTOR, value="button")
-                button.click()
+                try:
+                    button = c.find_element(By.CSS_SELECTOR, value="button")
+                    if button.text.lower() == "booked":
+                        print("Already Booked!")
+                        booked_waitlisted += 1
+                        break
+                    elif button.text.lower() == "waitlisted":
+                        print("Already Waitlisted!")
+                        booked_waitlisted += 1
+                        break
+                    elif button.text.lower() == "book class":
+                        button.click()
+                        print("Booked a class for Tue, 6 PM")
+                        classes_booked += 1
+                    elif button.text.lower() == "join waitlist":
+                        button.click()
+                        print("Joined waiting list for class on Tue, 6 PM")
+                        waitlists_joined += 1
+                except NoSuchElementException:
+                    print("found the Tue 6 PM class, but couldn't book for some reason...")
                 break
         break
+
+print(f"--- BOOKING SUMMARY ---\nClasses booked: {classes_booked}\nWaiting lists joined: {waitlists_joined}\nAlready Booked/Wait-listed: {booked_waitlisted}")
+
 
 input("enter to quit")
 driver.quit()
