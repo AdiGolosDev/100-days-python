2b8fdfa17c4b897fe0158bcd36b7d27785b2c277
Adi Golos <golos.adi.03@gmail.com>
2026-08-16 13:21:58 +0200

    completed day 57 blog website; practiced more jinja

--- Day 41 - 60/Day 57 jinja/project/main.py
+++ Day 41 - 60/Day 57 jinja/project/main.py
@@ -1,24 +1,5 @@
 from flask import Flask, render_template
-import random
-from datetime import date
-# import requests
 
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    random_number = random.randint(1,10)
-    current_year = date.today().year
-    return render_template("index.html", num=random_number, yr=current_year)
-
-# @app.route("/guess/<string:name>")
-# def guess(name):
-#     response_gender = requests.get("https://api.genderize.io", params={"name": name}).json()['gender']
-#     response_age = requests.get("https://api.agify.io", params={"name": name}).json()['age']
-
-#     return render_template("guess.html", age=response_age, gender=response_gender)
-
-# too lazy to put this in an actual json file
 all_posts = [
   {
     "id": 0,
@@ -52,14 +33,20 @@ all_posts = [
   }
 ]
 
-@app.route("/blog/<num>")
-def get_blog(num):
-    # blog_url = "https://www.npoint.io/docs/853efa5c4628642962ce"
-    # response = requests.get(blog_url)
-    # print(response.json())
-    # all_posts = response.json()
-    return render_template("blog.html", posts=all_posts)
+app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return render_template("index.html", posts=all_posts)
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    req_post = None
+    for post in all_posts:
+        if post["id"] == post_id:
+            req_post = post
+            break
+    return render_template("post.html", post=req_post)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
