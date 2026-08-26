c7c08519917d86ad0c9dab196bbacc62927a7a37
Adi Golos <golos.adi.03@gmail.com>
2026-08-26 16:27:43 +0200

    made simple book tracker website and data persistance with json file

--- Day 61 - 80/Day 63 sqlite-intro/main.py
+++ Day 61 - 80/Day 63 sqlite-intro/main.py
@@ -1,16 +1,35 @@
 from flask import Flask, render_template, request, redirect, url_for
+import json
+import os
+
+if os.path.exists("books.json") and os.path.getsize("books.json") > 0:
+    with open("books.json", "r") as f:
+        all_books = json.load(f)
+else:
+    all_books = []
 
 app = Flask(__name__)
 
-all_books = []
-
 @app.route('/')
-def home():
-    pass
+def index():
+    return render_template('index.html', books=all_books)
 
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add():
-    pass
+    if request.method == "POST":
+        new_book = {
+            "name": request.form["name"],
+            "author": request.form["author"],
+            "rating": request.form["rating"],
+        }
+        all_books.append(new_book)
+
+        with open("books.json", "w") as f:
+            json.dump(all_books, f, indent=2)
+
+        return redirect(url_for('index'))
+
+    return render_template('add.html')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
